6ce285bad24444ca7fa85fd09cc055e58044b6c7
Max McDonough <max.mcdonough@gmail.com>
2022-12-27 20:00:09 -0500

    Conitionals and booleans

--- alice.py
+++ alice.py
@@ -1,4 +1,10 @@
-name = 'Alice'
+name = 'Bob'
+age = 3000
 if name == 'Alice':
     print('Hi Alice')
-print('Done')
+elif age < 12:
+    print('You are not Alice!')
+elif age > 2000:
+    print('Unlike you, Alice is not immortal!')
+else:
+    print('You are not Alice, grandma')
