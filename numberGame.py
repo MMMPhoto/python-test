ac4fe8af5a3e8a81b8a3866ef54a5bc822805061
Max McDonough <max.mcdonough@gmail.com>
2022-12-28 23:05:17 -0500

    Working game with for loop conditional

--- numberGame.py
+++ numberGame.py
@@ -1,5 +1,6 @@
 # Guess the number game
 import random
+import sys
 
 # Ask player for name
 print('Hello, what is your name?')
@@ -7,4 +8,21 @@ name = input()
 
 # Explain the game
 print(f'Well, {name}, I am thinking of a number between 1 and 20.')
-print('Take a guess.')
+
+# Assign random number
+number = random.randint(1, 20)
+
+# Give player 6 guesses
+for num in range(6):
+    print('Take a guess.')
+    guess = input()
+
+    if int(guess) == int(number):
+        print(f'Good job, {name}! You guessed the number in {num + 1} tries')
+        break
+    elif int(guess) > int(number):
+        print('Your guess is too high.')
+        continue
+    elif int(guess) < int(number):
+        print('Your guess is too low.')
+        continue
