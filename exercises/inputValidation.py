ecaf7fe9de661b1eb42f3910b6f6e4d85a390ae4
Max McDonough <max.mcdonough@gmail.com>
2022-12-28 22:28:44 -0500

    Additional error handling

--- exercises/inputValidation.py
+++ exercises/inputValidation.py
@@ -1,11 +1,11 @@
 print('How many cats do you own?')
 numCats = input()
 try:
-    if int(numCats) <= 0:
-        raise Exception('No numbers below zero.')
-    elif int(numCats) >= 4:
+    if int(numCats) >= 4:
         print('That is a lot of cats!')
-    else:
+    elif int(numCats) >= 0:
         print('That is not that many cats.')
+    elif int(numCats) < 0:
+        print('You must enter a postive number.')
 except ValueError:
     print('You did not enter an integer.')
