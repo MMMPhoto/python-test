19076ef427603098b84d7f2e650d71e356b7bdfc
Max McDonough <max.mcdonough@gmail.com>
2022-12-28 22:46:14 -0500

    game explanation

--- numberGame.py
+++ numberGame.py
@@ -4,3 +4,7 @@ import random
 # Ask player for name
 print('Hello, what is your name?')
 name = input()
+
+# Explain the game
+print(f'Well, {name}, I am thinking of a number between 1 and 20.')
+print('Take a guess.')
