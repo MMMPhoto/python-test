240c62dfb8eba390518c744e663eb985ee09ae74
Max McDonough <max.mcdonough@gmail.com>
2023-01-03 17:24:58 -0500

    Initial file setup, use structure code from number game

---
+++ tic-tac-toe-game.py
@@ -0,0 +1,67 @@
+# Tic Tac Toe game
+import random
+import sys
+
+# Introduce player to game
+print('Hello, welcome to the Tic Tac Toe game!')
+
+# Ask player X or O
+print('Would you like to be X or O?')
+playerTurn = input()
+
+# Game function
+def ticTacToeGame():
+    # # Explain the game
+    # print(f'{name}, I am thinking of a number between 1 and 20.')
+
+    # # Assign random number
+    # secretNumber = random.randint(1, 20)
+
+    # # Give player 6 guesses
+    # for guesses in range(6):
+    #     guess = 0
+    #     if guesses > 0:
+    #         print(f'Number of guesses remaining: {6 - guesses}')
+
+    #     # Input validation
+    #     while True:
+    #         try:
+    #             guess = int(input('Take a guess: '))
+    #             while guess < 1 or guess > 20:
+    #                 print('You must enter an integer between 1 and 20.')
+    #                 guess = int(input('Take a guess: '))
+    #         except ValueError:
+    #             print('You must enter an integer between 1 and 20.')
+    #             continue
+    #         else:
+    #             break
+        
+    #     # Check against random value
+    #     if guess == secretNumber:
+    #         break
+    #     elif guess > secretNumber:
+    #         print('Your guess is too high.')
+    #     elif guess < secretNumber:
+    #         print('Your guess is too low.')
+
+    # # Either too many tries or correct guess
+    # if guess == secretNumber:
+    #     print(f'Good job, {name}! You guessed the number in {guesses + 1} tries')
+    # else:
+    #     print(f"Too bad! You couldn't guess in time. The number I was thinking of was {secretNumber}!")
+
+    # Ask to play again
+    print('Would you like to play again? (Y/N)')
+    playAgain = 0
+    while True:
+        playAgain = input()
+        if playAgain == 'Y':
+            ticTacToeGame()
+        elif playAgain == 'N':
+            print('Okay, goodbye!')
+            sys.exit()
+        else:
+            print('Please answer Y/N')
+            continue
+
+ticTacToeGame()
