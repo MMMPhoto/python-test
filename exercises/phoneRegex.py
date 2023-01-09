4cf92c26bc01f721a7154e1d24cd061d2ffd7011
Max McDonough <max.mcdonough@gmail.com>
2023-01-09 17:13:21 -0500

    Fixed grouping issues

--- exercises/phoneRegex.py
+++ exercises/phoneRegex.py
@@ -1,18 +1,19 @@
 import re
 
-phoneNumRegex = re.compile(r'(\(?\d{3}\)?)?(-|\.| )?(\d{3}(-|\.| )?\d{4})')
+phoneNumRegex = re.compile(r'\(?(\d{3})?\)?(?:-|\.| )?(\d{3}(?:-|\.| )?\d{4})')
 
 ()
 def searchForPhoneNumber():
-    matchObject = phoneNumRegex.search(input('Enter a phone number: '))
-    if matchObject != None:
-        print('Phone number found:')
-        i = 0
-        groups = len(matchObject.groups())
-        for i in range(groups + 1):
-            print(matchObject.group(i))
-    else:
-        print('No phone number found')
+    matchObject = phoneNumRegex.findall(input('Enter a phone number: '))
+    print(matchObject)
+    # if matchObject != None:
+    #     print('Phone number found:')
+    #     i = 0
+    #     groups = len(matchObject.groups())
+    #     for i in range(groups + 1):
+    #         print(matchObject.group(i))
+    # else:
+    #     print('No phone number found')
     searchForPhoneNumber()
 
 searchForPhoneNumber()
