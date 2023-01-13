150b67c28650988b063803f43af7e07df468ff40
Max McDonough <max.mcdonough@gmail.com>
2023-01-12 19:32:01 -0500

    Amazon selenium test

---
+++ exercises/amazonTest.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()
+browser.get('https://www.amazon.com/Automate-Boring-Stuff-Python-2nd-ebook/dp/B07VSXS4NK/')
+elems = browser.find_elements(By.CSS_SELECTOR, '#kindle-price')
+
+price = elems[0].text
+
+print(price)
