baf0670addda6e2c2519573e6f719ff9c0faf186
Max McDonough <max.mcdonough@gmail.com>
2023-01-12 19:24:49 -0500

    Selenium test

---
+++ exercises/seleniumTest.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()
+browser.get('https://mmmphoto.github.io/Development-Portfolio/')
+elems = browser.find_elements(By.TAG_NAME, "p")
+
+aboutText = ''
+for i in range(len(elems) - 1):
+    aboutText += f'{aboutText}{elems[i].text} '
+print(aboutText)
